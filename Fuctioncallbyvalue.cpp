49996338f067de870dee29900a79325361771a0b
Yanwar Arief <ariefyanwar20@gmail.com>
2017-03-04 16:21:32 +0700

    Add files via upload

--- Fuctioncallbyvalue.cpp
+++ Fuctioncallbyvalue.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
-#include "swaplib.h" // Memasukan "swaplib.h" kedalam program
 using namespace std;
-//I.S Program Memasukan Angka untuk di proses
-//F.S Program Menampilkan data yang telah di proses dalam swaplib.h
+//I.S Program Memasukan Data
+//F.S Program Menampilkan data yang sudah di proses
+
+//Prototype
+void swap(int x, int y);
 
 int main () {
+
 	//Kamus
 	int a = 100;
 	int b = 200;
-	//Algoritma
 
-	//Tugas 1
+	//Output 1
 	cout <<"before swap,value of a :" << a << endl;
 	cout <<"before swap,value of b :" << b << endl;
 
-	//Tugas 2
+	//Tugas 1
 	swap (a, b);
 
+	// Output 2
 	cout <<"after swap,value of a :" << a << endl;
 	cout <<"after swap,value of b :" << b << endl;
 	return 0;
+}
+
+void swap(int x, int y) {
+	//Kamus
+	int temp;
+
+	//Proses Tugas 1
+	temp = x;
+	x = y;
+	y = temp;
+	return;
 }
---
+++ chararraystring.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//I.S Program menunggu memasukan angka
+//F.S Program menampilkan angka yang sudah di proses
+
+int main (){
+	string nama = "Yanwar";
+	char nama2 [] = "Yanwar";
+	char nama3 [6] = {'Y', 'A', 'N', 'W', 'A', 'R'};
+
+	cout << nama << endl;
+	cout << nama2 << endl;
+	cout << nama3 [3] << endl;
+	return 0;
+}

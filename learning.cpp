14ba29c7bc0bd03eeccf19773abe09c09163d6a4
Yanwar Arief <ariefyanwar20@gmail.com>
2017-03-26 12:05:34 +0700

    Add files via upload

---
+++ learning.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+// {i.s} Inisialisasi Fungsi
+// {f.s} Pemanggilan Fungsi
+using namespace std;
+
+// Inisialisasi Fungsi
+ void contoh() {
+  cout << "Ini Hanya Contoh " << endl;
+
+}
+
+//Inisialisai Fungsi
+ void Nama(string nama) {
+  cout << "Hallo Bro " << nama << endl;
+}
+
+int main(){
+
+  contoh(); // pemanggilan fungsi
+
+
+  Nama("Yanwar Arief"); // pemanggilan fungsi
+
+  cout << "Welcome" << endl;
+
+  return 0;
+}
